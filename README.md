798660c470b856046c0efdb9f5c180bb6575c9e2
Oっぱい <44335683+Nxlllp@users.noreply.github.com>
2018-12-05 00:20:35 +0900

    add epexp

--- README.md
+++ README.md
@@ -1,23 +1,13 @@
-# Talents Info
+# 特性の奴
 
-Shows talents info **when you open talents game window** or on **!EP** proxy command
-Also shows **EXP +N** messages when you receive talents experience points along with DailyCap limit if it wasn't reached yet and warns you when it gets reached
+勝手に弄った
 
-<img src=http://u.cubeupload.com/Owyn/talentinfo.png>
+取得したら特性経験値が見える
 
-# FAQ:
+## 元の奴からの変更点
 
-**Q.** - my level isn't the same as this modules shows?!?!
+見りゃわかる
 
-**A.** - well, first of all - game doesn't even show your level anywhere in the game in the first place
-see this document for more info: https://docs.google.com/spreadsheets/d/e/2PACX-1vTy7_5TYA_WUd--cFqYeGFm0mnb9oaCzcar3yH3FgkQD6y-mRD7id9k97ttrMbkjjId8ly-aBWzexZt/pubhtml?gid=5547005
+獲得してる経験値、次の必要経験値、そのパーセントを表示するようにした
 
-
-**Q.** - I haven't done anything today but my dailyexp isn't zero already!!?
-
-**A.** - It resets once a day, when? - No idea, there is no information about that built-in the game.
-
-
-**Q.** - my cap isn't the same as in the document above?!?!
-
-**A.** - yes, it's 89% of that cap, after that your gained EXP gets reduced noticeably, see this for more info why https://docs.google.com/document/d/16wuje8nV7yYd0y2gq3oVFbjpR2Ulx4SQRWQQkK-LVrs
+プログラミングはあんましたことないから追加したコードは汚いかもしれないし汚いかもしれない
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+const epexpp = require('./epexp.json');
+
 module.exports = function talentsinfo(mod) {
 	const command = mod.command || mod.require.command;
-	const softcap = 0.8901403358192;
 	let warned = false;
 	let lvl = 0,
 		exp = 0,
 		dexp = 0,
-        dcap = 0,
-		sdcap = 0;
+		dcap = 0;
 
     // message on command
     command.add(['talent', 'talents', 'EP', '!EP'], msg);
@@ -14,47 +14,62 @@ module.exports = function talentsinfo(mod) {
     // send message exp/cap (exp%)
     function msg()
 	{
-        command.message(`<font color="#FDD017">info:</font> LVL <font color="#00FFFF">${lvl}</font>, EXP: <font color="#00FFFF">${exp}</font>, DailyEXP <font color="#00FFFF">${dexp}/${sdcap} (${Math.round(100*dexp/sdcap)}%) </font>`);
+        command.message(`<font color="#FDD017">Talents info:</font> LVL <font color="#00FFFF">${lvl}</font>, EXP: <font color="#00FFFF">${exp}</font>, soft DailyEXP <font color="#00FFFF">${dexp}/${sdcap()} (${Math.round(100*dexp/sdcap())}%)</font>`);
 	}
 	
-	mod.hook('S_LOAD_EP_INFO', 1, event=>{
+	// calc sdcap
+	function sdcap()
+	{
+		const softcap = 0.8901403358192;
+		return Math.floor(dcap * softcap);
+	}	
+	
+	mod.hook('S_LOAD_EP_INFO', 1, event => {
 		exp = event.exp;
 		lvl = event.level;
 		dexp = event.dailyExp;
 		dcap = event.dailyExpMax;
-		sdcap = Math.floor(dcap*softcap);
 	});
-	
-	mod.hook('S_CHANGE_EP_EXP_DAILY_LIMIT', 1, event=>{
-		dcap = event.limit;
-		sdcap = Math.floor(dcap*softcap);
-	});
-	
-	mod.hook('S_PLAYER_CHANGE_EP', 'raw', (code, data)=>{
-		let gained = data.readInt32LE(4);
-		exp = data.readInt32LE(8); // 64 actually but this should be enough
-		lvl = data.readInt32LE(16);
-		dexp = data.readInt32LE(20);
-		dcap = data.readInt32LE(24);
-		sdcap = Math.floor(dcap*softcap);
-		let scmod = Math.round(data.readFloatLE(37) * 100);
-		if(gained)
+
+	mod.hook('S_PLAYER_CHANGE_EP', 1, event => {
+		gained = event.expDifference;
+		exp = event.exp;
+		lvl = event.level;
+		dexp = event.dailyExp;
+		dcap = event.dailyExpMax;
+		scmod = Math.round(event.tsRev * 100);
+	if (gained)
+	{
+		if (dexp >= sdcap())
 		{
-			if(dexp >= sdcap)
-			{
-				if(!warned)
+			if(!warned)
 				{
-					command.message('<font color="#FDD017">EXP</font> Daily Cap <font color="#FF0000">reached!</font>');
-					warned = true;
+					command.message('<font color="#FDD017">Talents EXP</font> Daily SoftCap <font color="#FF0000">reached!</font>');
+					warned = true;	  
 				}
-			}
-			else
-			{
-				warned = false;
-			}
-			command.message('<font color="#00FFFF">+' + gained + ' EXP</font>' + (!warned ? ' (' + dexp + ' / ' + sdcap + ' (Daily Cap), <font color="#FFF380">' + (sdcap-dexp) + '</font> EXP left for today uncapped)' : ' (' + scmod + '% mod)' ));
+		}	else	{
+		warned = false;
 		}
+      let message = `<font color="#00FFFF">+${gained} EXP</font>`;
+      if (warned)
+    {
+        message += `(${scmod}% mod)`;
+	}	else	{
+        message += `(${dexp}/${sdcap()}) (Daily Cap), <font color="#FFF380">${sdcap()} - ${dexp}</font> EXP left for today uncapped)`;
+    }
+      command.message(message);
+		if (epexp = epexpp.filter(b => b.level === lvl)[0]) {
+		  epp = epexp.exp;
+		if (epexp2 = epexpp.filter(b => b.level === lvl -1)[0]) {
+		  epp2 = epexp2.exp;
+		  //mmm = exp;
+		 mmm = Math.round((Number(exp)-epp2)/(epp-epp2)*1000) / 10;
+			mod.command.message(`<font color="#FDD017"> EXP : </font><font color="#00FFFF">${exp} / ${epp} (${mmm}%)</font>`);
+		};};	  
+	  
+    }
 	});
+
 	
 	// open EP ui
     mod.hook('C_REQUEST_CONTRACT', 1, event => {
